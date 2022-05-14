a71e32335df57b697b91b312288e82936f2d571a
Mahmud-007 <mahmud6799@gmail.com>
2022-05-14 09:58:36 +0600

    add homepage

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,11 +1,103 @@
-import type { NextPage } from 'next'
-
-const Home: NextPage = () => {
+import React from "react";
+import { NextPage } from "next";
+import {
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+  Button,
+} from "@material-ui/core";
+import NextLink from "next/link";
+const Home: NextPage = ({ products }) => {
   return (
     <div>
-      Home
+      <h1>Products</h1>
+      <Grid container spacing={3}>
+        {products.map(
+          (product: {
+            name:
+              | boolean
+              | React.ReactElement<
+                  any,
+                  string | React.JSXElementConstructor<any>
+                >
+              | React.ReactFragment
+              | React.Key
+              | null
+              | undefined;
+            slug: any;
+            image: string | undefined;
+            price:
+              | string
+              | number
+              | boolean
+              | React.ReactElement<
+                  any,
+                  string | React.JSXElementConstructor<any>
+                >
+              | React.ReactFragment
+              | React.ReactPortal
+              | null
+              | undefined;
+          }) => (
+            <Grid item md={4} key={product.name}>
+              <NextLink href={`/product/${product.slug}`} passHref>
+                <Card>
+                  <CardActionArea>
+                    <CardMedia
+                      component="img"
+                      image={product.image}
+                      title={product.name}
+                    ></CardMedia>
+                    <CardContent>
+                      <Typography>{product.name}</Typography>
+                    </CardContent>
+                  </CardActionArea>
+                  <CardActions>
+                    <Typography>${product.price}</Typography>
+                    <Button size="small" color="primary">
+                      Add to cart
+                    </Button>
+                  </CardActions>
+                </Card>
+              </NextLink>
+            </Grid>
+          )
+        )}
+      </Grid>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
+
+export const getServerSideProps = async () => {
+  const products = [
+    {
+      image: "https://www.scorebat.com/og/m/og1152402.jpeg",
+      name: "first",
+      price: 100,
+      slug: null
+    },
+    {
+      image: "https://www.scorebat.com/og/m/og1152402.jpeg",
+      name: "first",
+      price: 100,
+      slug: null
+    },
+    {
+      image: "https://www.scorebat.com/og/m/og1152402.jpeg",
+      name: "first",
+      price: 100,
+      slug: null
+    },
+  ];
+  return {
+    props: {
+      products: products,
+    },
+  };
+};
