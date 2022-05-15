43d7879da0fd1d61aed7c7e04205736b72b31e34
Mahmud-007 <mahmud6799@gmail.com>
2022-05-15 07:01:07 +0600

    add Layout component

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -11,64 +11,67 @@ import {
   Button,
 } from "@material-ui/core";
 import NextLink from "next/link";
+import Layout from "../components/Layout";
 const Home: NextPage = ({ products }) => {
   return (
-    <div>
-      <h1>Products</h1>
-      <Grid container spacing={3}>
-        {products.map(
-          (product: {
-            name:
-              | boolean
-              | React.ReactElement<
-                  any,
-                  string | React.JSXElementConstructor<any>
-                >
-              | React.ReactFragment
-              | React.Key
-              | null
-              | undefined;
-            slug: any;
-            image: string | undefined;
-            price:
-              | string
-              | number
-              | boolean
-              | React.ReactElement<
-                  any,
-                  string | React.JSXElementConstructor<any>
-                >
-              | React.ReactFragment
-              | React.ReactPortal
-              | null
-              | undefined;
-          }) => (
-            <Grid item md={4} key={product.name}>
-              <NextLink href={`/product/${product.slug}`} passHref>
-                <Card>
-                  <CardActionArea>
-                    <CardMedia
-                      component="img"
-                      image={product.image}
-                      title={product.name}
-                    ></CardMedia>
-                    <CardContent>
-                      <Typography>{product.name}</Typography>
-                    </CardContent>
-                  </CardActionArea>
-                  <CardActions>
-                    <Typography>${product.price}</Typography>
-                    <Button size="small" color="primary">
-                      Add to cart
-                    </Button>
-                  </CardActions>
-                </Card>
-              </NextLink>
-            </Grid>
-          )
-        )}
-      </Grid>
-    </div>
+    <Layout>
+      <div>
+        <h1>Products</h1>
+        <Grid container spacing={3}>
+          {products.map(
+            (product: {
+              name:
+                | boolean
+                | React.ReactElement<
+                    any,
+                    string | React.JSXElementConstructor<any>
+                  >
+                | React.ReactFragment
+                | React.Key
+                | null
+                | undefined;
+              slug: any;
+              image: string | undefined;
+              price:
+                | string
+                | number
+                | boolean
+                | React.ReactElement<
+                    any,
+                    string | React.JSXElementConstructor<any>
+                  >
+                | React.ReactFragment
+                | React.ReactPortal
+                | null
+                | undefined;
+            }) => (
+              <Grid item md={4} key={product.name}>
+                <NextLink href={`/product/${product.slug}`} passHref>
+                  <Card>
+                    <CardActionArea>
+                      <CardMedia
+                        component="img"
+                        image={product.image}
+                        title={product.name}
+                      ></CardMedia>
+                      <CardContent>
+                        <Typography>{product.name}</Typography>
+                      </CardContent>
+                    </CardActionArea>
+                    <CardActions>
+                      <Typography>${product.price}</Typography>
+                      <Button size="small" color="primary">
+                        Add to cart
+                      </Button>
+                    </CardActions>
+                  </Card>
+                </NextLink>
+              </Grid>
+            )
+          )}
+        </Grid>
+      </div>
+    </Layout>
   );
 };
 
@@ -80,19 +83,19 @@ export const getServerSideProps = async () => {
       image: "https://www.scorebat.com/og/m/og1152402.jpeg",
       name: "first",
       price: 100,
-      slug: null
+      slug: null,
     },
     {
       image: "https://www.scorebat.com/og/m/og1152402.jpeg",
       name: "first",
       price: 100,
-      slug: null
+      slug: null,
     },
     {
       image: "https://www.scorebat.com/og/m/og1152402.jpeg",
       name: "first",
       price: 100,
-      slug: null
+      slug: null,
     },
   ];
   return {
